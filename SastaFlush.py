057166a2e61b1f360545aab691548f7aba848984
SwaritMisro12 <95137718+SwaritMisro12@users.noreply.github.com>
2022-12-04 15:15:57 +0530

    Add files via upload

---
+++ SastaFlush.py
@@ -0,0 +1,59 @@
+import discord
+import random
+import os
+import AiinLen
+
+
+
+
+
+
+command.trigger("$Ping").all()
+command.trigger("$Ban").specfic.users() in case("1")
+command.trigger("$hello").all()
+command.trigger("$kick").specfic.users() in case("1")
+
+
+discord.system = @event.client()
+
+
+def intro("Intro") 
+    if intro.rep = reco input.trigger() 
+           elif 
+               trigger("$Intro").all
+                 print("?Any Information Want To Known About This Bot!")
+              if  sub.command("^Information").auto = reco
+                       print("This For The Moderation Purpose And Under Development Of AntiCheat!")
+                    return; 
+
+
+in @event.client()
+def ping("Ping")   
+          int = a("Ping!")
+           int = b("Pong!")
+             if a is value("Ping!") 
+                   in b ("Pong!")
+                    print.a("Ping!"),b("Pong!")
+                       return;
+
+in @event.client()
+def Ban("Ban")
+     int user = discord.roles("Everyone") 
+        if users = ("1").case + role(removed.all) + Permission(removed.all)
+            forward.specfic + users{"Bye Man Never See You Again"}
+                 unforward.specfic + users{in case(days.discord).refowarded} = print("Welcome back To Sever If Your Unbaned!")
+                 return;
+
+in @event.client()
+def hello("hello")
+   int a = user in discord
+      int b = system.send(all.client)
+         print("Welcome To Your Discord Server! If You Want Any Information About Our Bot/AntiCheat Pls Type this command($help)")
+             return;
+
+             
+
+
+
+
+
